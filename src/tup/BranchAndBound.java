50280980a8fa6abd84628d104be2e675d1ca57b1
giniloc <gil.nimmegeers1@gmail.com>
2024-03-17 19:29:18 +0100

    Revert "poging"
    This reverts commit 785c780e356c67e3fbf12bd7466ba653a1c6cc9b.

--- src/tup/BranchAndBound.java
+++ src/tup/BranchAndBound.java
@@ -22,10 +22,10 @@ public class BranchAndBound {
         }
 
         // Voer het branch-and-bound algoritme uit vanaf de tweede ronde
-        branchAndBound(path, 1, 1);
+        branchAndBound(path, 0, 1, 0);
     }
 
-    private void branchAndBound(int[][] path, int umpire, int round) {
+    private void branchAndBound(int[][] path, int umpire, int round, int distance) {
         int uPlus = (umpire % problem.nTeams) + 1; //umpire te bekijken in volgende iteratie
         int rPlus;
         if (umpire + 1 == problem.nTeams) {
@@ -35,11 +35,11 @@ public class BranchAndBound {
         List<Integer> A = problem.getFeasibleAllocations(umpire, round);
         for (Integer a : A) {
             if (!problem.canBePruned(a)) {
-                path[umpire-1][round] = a;
+                path[umpire][round] = a;
                 if (round < problem.nTeams - 1) {
-                    branchAndBound(path, uPlus, rPlus);
+                    branchAndBound(path, uPlus, rPlus, distance + problem.dist[umpire][a]);
                 } else {
-                    int totalDistance = calculateTotalDistance(path);//Hier moet local search worden gedaan
+                    int totalDistance = calculateTotalDistance(path);
                     if (totalDistance < bestDistance) {
                         bestDistance = totalDistance;
                         for (int i = 0; i < problem.nTeams; i++) {
@@ -47,10 +47,9 @@ public class BranchAndBound {
                         }
                     }
                 }
-                path[umpire-1][round] = -1;
+                path[umpire][round] = -1;
             }
         }
-        printPath(path);
     }
     private int calculateTotalDistance(int[][] path) {
         int totalDistance = 0;
@@ -61,11 +60,6 @@ public class BranchAndBound {
         }
         return totalDistance;
     }
-    private void printPath(int[][] path) {
-        System.out.println("-------------------------------------------------------------------------------------");
-        System.out.println(Arrays.deepToString(path));
-    }
-
 
 
 
