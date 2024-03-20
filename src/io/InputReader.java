7dcfd2dea095e50b4be5ca0b007ea8ec387a204c
giniloc <gil.nimmegeers1@gmail.com>
2024-03-20 19:15:07 +0100

    fixed bug
    input readen fix

--- src/io/InputReader.java
+++ src/io/InputReader.java
@@ -9,31 +9,41 @@ import java.util.Scanner;
 
 public class InputReader {
     public static Problem readFile(String fileName, int q1, int q2) throws FileNotFoundException {
-        File file = new File(new File(fileName).getAbsolutePath());
+        File file = new File(new File("src\\io\\input\\" + fileName).getAbsolutePath());
         // Read the file
         Scanner scanner = new Scanner(file);
+        String line;
 
-        scanner.nextLine(); // \n
-        int nTeams = Integer.parseInt(scanner.nextLine().split("=")[1].split(";")[0]);
+        line = removePadding(scanner, "nTeams");
+        assert line != null;
+        int nTeams = Integer.parseInt(line.split("=")[1].split(";")[0]);
 
-        scanner.nextLine(); // \n
-        scanner.nextLine(); // dist= [
-        int[][] dist = new int[nTeams][nTeams];
-        for (int i = 0; i < nTeams; i++) {
-            String line = scanner.nextLine().split("\\[")[1].split("]")[0];
-            dist[i] = Arrays.stream(line.trim().split("\\s+" )).mapToInt(Integer::parseInt).toArray();
-        }
+        removePadding(scanner, "dist");
+        int[][] dist = parseArray(scanner, nTeams);
 
-        scanner.nextLine(); // ];
-        scanner.nextLine(); // \n
-        scanner.nextLine(); // opponents= [
-        int[][] opponents = new int[nTeams][nTeams];
-        for (int i = 0; i < nTeams; i++) {
-            String line = scanner.nextLine().split("\\[")[1].split("]")[0];
-            opponents[i] = Arrays.stream(line.trim().split("\\s+" )).mapToInt(Integer::parseInt).toArray();
-        }
+        removePadding(scanner, "opponents");
+        int[][] opponents = parseArray(scanner, (nTeams - 1) * 2);
 
         scanner.close();
         return new Problem(nTeams, dist, opponents, q1, q2);
+    }
+
+    private static int[][] parseArray(Scanner scanner, int nTeams) {
+        int[][] array = new int[nTeams][nTeams];
+        for (int i = 0; i < nTeams; i++) {
+            String line = scanner.nextLine().split("\\[")[1].split("]")[0];
+            array[i] = Arrays.stream(line.trim().split("\\s+" )).mapToInt(Integer::parseInt).toArray();
+        }
+        return array;
+    }
+
+    private static String removePadding(Scanner scanner, String end) {
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.contains(end)) {
+                return line;
+            }
+        }
+        return null;
     }
 }
--- src/tup/Problem.java
+++ src/tup/Problem.java
@@ -8,11 +8,15 @@ public class Problem {
     final int q1;
     final int q2;
     final int nTeams;
+    public final int nUmpires;
+    public final int nRounds;
     final int[][] dist;
     final int[][] opponents;
 
     public Problem(int nTeams, int[][] dist, int[][] opponents, int q1, int q2) {
         this.nTeams = nTeams;
+        this.nUmpires = nTeams / 2;
+        this.nRounds = (nTeams - 1) * 2;
         this.dist = dist;
         this.opponents = opponents;
         this.q1 = q1;
@@ -22,14 +26,38 @@ public class Problem {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Problem{");
-        sb.append("q1=").append(q1);
-        sb.append(",\n q2=").append(q2);
-        sb.append(",\n nTeams=").append(nTeams);
-        sb.append(",\n dist=").append(Arrays.deepToString(dist));
-        sb.append(",\n opponents=").append(Arrays.deepToString(opponents));
-        sb.append('}');
+        sb.append("Problem{\n");
+        sb.append("q1=").append(q1).append(",\n");
+        sb.append("q2=").append(q2).append(",\n");
+        sb.append("nTeams=").append(nTeams).append(",\n");
+        sb.append("nUmpires=").append(nUmpires).append(",\n");
+
+        sb.append("dist=[\n");
+        buildArray(sb, dist);
+        sb.append("]\n");
+
+        sb.append("opponents=[\n");
+        buildArray(sb, opponents);
+        sb.append("]\n");
+
+        sb.append("}");
         return sb.toString();
+    }
+    private static void buildArray(StringBuilder sb, int[][] array2D) {
+        int maxWidth = 0;
+        for (int[] array1D : array2D) {
+            for (int element : array1D) {
+                int width = String.valueOf(element).length();
+                if (width > maxWidth) maxWidth = width;
+            }
+        }
+        for (int[] array1D : array2D) {
+            sb.append("[");
+            for (int element : array1D) {
+                sb.append(String.format("%" + (maxWidth + 2) + "s", element));
+            }
+            sb.append("]\n");
+        }
     }
     public void assignUmpires() {
         int[][] assignments = new int[nTeams][2*nTeams - 2];
@@ -42,10 +70,10 @@ public class Problem {
         // Wijs scheidsrechters toe voor de resterende rondes
         for (int round = 1; round < 2*nTeams - 2; round++) {
             for (int umpire = 0; umpire < nTeams; umpire++) {
-                List<Integer> feasibleAllocations = getFeasibleAllocations(umpire, round, assignments);
+                List<Integer> feasibleAllocations = getValidAllocations(umpire, round, assignments);
                 if (!feasibleAllocations.isEmpty()){
                     for (Integer allocation : feasibleAllocations) {
-                        assignments[umpire][round] = Math.abs(opponents[umpire][allocation]);
+                        assignments[umpire][round] = allocation;
                         break;
                     }
                 }
@@ -60,34 +88,27 @@ public class Problem {
         return false;
     }
 
-
-    public List<Integer> getFeasibleAllocations(int umpire, int round, int[] [] assignments) {
+    private List<Integer> getValidAllocations(int umpire, int round, int[][] assignments) {
         List<Integer> feasibleAllocations = new ArrayList<>();
-        for (int i = 0; i < nTeams; i++) {
-            if (isValidAllocation(umpire, round, i, assignments)) {
-                feasibleAllocations.add(i);
-            }
-        }
-        return feasibleAllocations;
-    }
-
-    private boolean isValidAllocation(int umpire, int round, int game, int[][] assignments) {
         // Check if the umpire has not visited the same location in the previous q1 - 1 rounds
-        for (int j =0; j< opponents.length;j++){
+        for (int j =0; j< nTeams-1;j++){
             for (int i = 1; i < q1 && round - i >= 0; i++) {
-                if (opponents[round][j] == assignments[umpire][round - i]) {
-                    return false;
+                if (Math.abs(opponents[round][j]) == assignments[umpire][round - i]) {
+                    continue;
                 }
+                feasibleAllocations.add(opponents[round][j]);
             }
             // Check if the umpire has not refereed any of the teams during the previous q2 - 1 rounds
+            /*
             for (int k = 1; k < q2 && round - k >= 0; k++) {//deze weet ik nog echt niet hoe ik moet doen
                 if (opponents[umpire][game] == opponents[umpire][round - k]) {
                     return false;
                 }
             }
+             */
         }
 
-        return true;
+        return feasibleAllocations;
     }
 
 }
