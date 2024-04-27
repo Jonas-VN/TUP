dbd242605b712054deeec79db5b2cfd667a3a085
giniloc <gil.nimmegeers1@gmail.com>
2024-04-27 15:02:12 +0200

    fixed B&Bsub
    fixed LB

--- src/tup/Main.java
+++ src/tup/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         Problem problem = InputReader.readFile("umps10.txt", 4, 2);
 //        System.out.println(problem);
-//        BranchAndBound bb = new BranchAndBound(problem);
-//        bb.solve();
-        LowerBound lb = new LowerBound(problem);
-        lb.solve();
+        BranchAndBound bb = new BranchAndBound(problem);
+        bb.solve();
+      //  LowerBound lb = new LowerBound(problem);
+      //  lb.solve();
 
 
 
