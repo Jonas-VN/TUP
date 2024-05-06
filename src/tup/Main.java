ab14e5f0d1fa642afa959363523d2c79ef9758cc
Jonas-VN <87768347+Jonas-VN@users.noreply.github.com>
2024-05-06 10:45:57 +0200

    prints & main opkuisen

--- src/tup/Main.java
+++ src/tup/Main.java
@@ -7,14 +7,12 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        String instance = "umps10";
-        int q1 = 5;
+        String instance = "umps14";
+        int q1 = 7;
         int q2 = 2;
         Problem problem = InputReader.readFile(instance + ".txt", q1, q2);
-        long StartTime = System.currentTimeMillis();
         BranchAndBound bb = new BranchAndBound(problem);
         bb.solve();
-        System.out.println("Total time: " + (System.currentTimeMillis() - StartTime) / 1000.0 + "s");
         OutputWriter.writeSolutionToFile(instance + "_" + q1 + "_" + q2 + ".txt", bb.bestSolution);
     }
 }
